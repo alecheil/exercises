d2a56c81afe930893750e884b1992a72ed7d7ce1
Alec Heil <alec-heil@student.kirkwood.edu>
2017-09-19 13:17:41 +0000

    adress prgrom

--- chapter-2/ex-2-1.py
+++ chapter-2/ex-2-1.py
@@ -6,19 +6,19 @@
 # It will display the name and contact information for a teacher.
 
 # display the full name
-
+print("Alec Heil")
 
 # display the address
-
+print("742 Evergreen Terrace")
 
 # display the city, state and zip
-
+print("Springfield, Illinois 62629")
 
 # display the phone number
-
+print("867-5309")
 
 # display the course area
-
+print("CIS-121")
 
 
 
